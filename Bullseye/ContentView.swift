3e78c8ff75bca765e24c3bd6cef69752069f470c
omrobbie <putu.roby@yahoo.com>
2019-09-26 12:07:15 +0800

    refactor: game score calculation

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -78,7 +78,19 @@ struct ContentView: View {
     }
 
     func pointsForCurrentRound() -> Int {
-        100 - amountOff()
+        let maximumScore = 100
+        let difference = amountOff()
+        let bonus: Int
+
+        if difference == 0 {
+            bonus = 100
+        } else if difference == 1 {
+            bonus = 50
+        } else {
+            bonus = 0
+        }
+
+        return maximumScore - difference + bonus
     }
 
     func alertTitle() -> String {
