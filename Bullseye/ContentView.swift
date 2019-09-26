4d7ba5fc315b807f0c1ffc27f15141e8b767d9a4
omrobbie <putu.roby@yahoo.com>
2019-09-26 10:30:02 +0800

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by omrobbie on 26/09/19.
+//  Copyright © 2019 omrobbie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
