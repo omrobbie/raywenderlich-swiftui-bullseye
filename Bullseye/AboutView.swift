1a0ff980dac87e26ecad13b393789cd5c7de2cf5
omrobbie <putu.roby@yahoo.com>
2019-09-26 12:58:14 +0800

    refactor about view content

--- Bullseye/AboutView.swift
+++ Bullseye/AboutView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Text("Hello World!")
+        VStack {
+            Text("🎯 Bullseye 🎯")
+            Text("This is Bullseye, the game where you can win points and earn fame by dragging a slider.")
+            Text("Your goal is to place the slider as close as possible to the target value. The closer you are, the more points you score.")
+            Text("Enjoy!")
+        }
+        .navigationBarTitle("About Bullseye")
     }
 }
 
