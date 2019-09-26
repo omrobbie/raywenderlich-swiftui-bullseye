9bca9cdf905e200262df1df388a4877d5403e201
omrobbie <putu.roby@yahoo.com>
2019-09-26 12:51:13 +0800

    add new view

---
+++ Bullseye/AboutView.swift
@@ -0,0 +1,21 @@
+//
+//  AboutView.swift
+//  Bullseye
+//
+//  Created by omrobbie on 26/09/19.
+//  Copyright © 2019 omrobbie. All rights reserved.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}
